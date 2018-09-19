64a58a25cf21418640452a4c061779f1727dfdec
Satya <satyagvns@outlook.com>
2018-09-20 01:25:54 +0530

    Testing Heroku

--- app/controllers/dashboards_controller.rb
+++
@@ -1,9 +0,0 @@
-class DashboardsController < ApplicationController
-  def index
-    email = session[:email]
-    if email.blank? 
-      redirect_to :controller => 'users',
-        :action => 'index'
-    end
-  end
-end
